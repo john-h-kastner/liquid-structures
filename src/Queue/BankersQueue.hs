4e8672d4e4103c4a7b3d10f03a2b3061e196081b
John Kastner <john.h.kastner@gmail.com>
2019-04-16 11:47:10 -0400

    Created queue typeclass instance for BankersQueue

--- src/Queue/BankersQueue.hs
+++ src/Queue/BankersQueue.hs
@@ -3,16 +3,18 @@ module Queue.BankersQueue where
 {- DONE:
  -   lenf >= lenr is ensured
  -   saftey of partial functions is checked
+ -   implement generic Queue typeclass
  -}
 
 {- TODO:
  -   content of queue is preserved
  -   queue order is presevered
- -   implement generic Queue typeclass
- -   write somee examples demonstrating checked properties
+ -   write some examples demonstrating checked properties
  -}
 
 import Prelude hiding (head, tail)
+
+import Queue.Queue
 
 {-@ data BankersQueue a = BQ {
       lenf :: Nat,
@@ -29,10 +31,6 @@ data BankersQueue a = BQ {
   r    :: [a]
 } deriving Show
 
-{-@ measure qlen @-}
-qlen :: BankersQueue a -> Int
-qlen (BQ f _ r _) = f + r
-
 {-@ check ::
     vlenf : Nat              ->
     {v:[_] | len v == vlenf} ->
@@ -47,31 +45,14 @@ check lenf f lenr r =
   else
     BQ (lenf + lenr) (f ++ (reverse r)) 0 []
 
-{-@ empty :: {q:BankersQueue _ | qlen q == 0} @-}
-empty :: BankersQueue a
-empty = BQ 0 [] 0 []
+instance Queue BankersQueue where
+  {-@ instance measure qlen :: BankersQueue a -> Int
+      qlen (BQ f _ r _) = f + r
+    @-}
 
-{-@ isEmpty :: q:BankersQueue _ -> {b:Bool | b <=> qlen q == 0} @-}
-isEmpty :: BankersQueue a -> Bool
-isEmpty (BQ lenf f lenr r) = (lenf == 0)
+  empty = BQ 0 [] 0 []
+  isEmpty (BQ lenf f lenr r) = (lenf == 0)
 
-{-@ snoc :: forall a.
-    q : BankersQueue a ->
-    e : a              ->
-    {q' : BankersQueue a | qlen q' == (qlen q) + 1}
-  @-}
-snoc :: BankersQueue a -> a -> BankersQueue a
-snoc (BQ lenf f lenr r) x = check lenf f (lenr + 1) (x : r)
-
-{-@ head :: forall a.
-    {q : BankersQueue a | qlen q /= 0} ->
-    a
-  @-}
-head :: BankersQueue a -> a
-head (BQ lenf (x : f') lenr r) = x
-
-{-@ tail :: forall a.
-    {q : BankersQueue a | qlen q /= 0} ->
-    {q' : BankersQueue a | qlen q' == (qlen q) - 1}
-  @-}
-tail (BQ lenf (x : f') lenr r) = check (lenf - 1) f' lenr r
+  snoc (BQ lenf f lenr r) x = check lenf f (lenr + 1) (x : r)
+  head (BQ lenf (x : f') lenr r) = x
+  tail (BQ lenf (x : f') lenr r) = check (lenf - 1) f' lenr r
---
+++ src/Queue/Queue.hs
@@ -0,0 +1,31 @@
+module Queue.Queue where
+
+import Prelude hiding (head, tail)
+
+{- Note: I wanted to have len return Nat, liquid haskell did not recognize the
+ - type. Nat is recognized elsewhere in this file, so this could be a problem
+ - with LiquidHaskell but, I haven't spent much time looking into this. -}
+
+{-@ class measure qlen :: forall a. a -> Int @-}
+
+{-@ class Queue q where
+      empty   :: forall a.
+        {q:(q a) | 0 == qlen q}
+      isEmpty :: forall a.
+        q:(q a) -> {v:Bool | v <=> (0 == qlen q)}
+
+      snoc    :: forall a.
+        q0:q a -> a -> {q1:q a | (qlen q1) == (qlen q0) + 1}
+      head    :: forall a.
+        {q:q a | qlen q /= 0} -> a
+      tail    :: forall a.
+        {q0:q a | qlen q0 /= 0} -> {q1:q a | (qlen q1) == (qlen q0) - 1}
+  @-}
+
+class Queue q where
+  empty   :: q a
+  isEmpty :: q a -> Bool
+
+  snoc    :: q a -> a -> q a
+  head    :: q a -> a
+  tail    :: q a -> q a
