d81152839ca83d7f358c826c3f9aa5d58b71331b
John Kastner <john.h.kastner@gmail.com>
2019-05-03 19:27:49 -0400

    Code cleanup can comments.

--- src/Set/Set.hs
+++ src/Set/Set.hs
@@ -100,26 +100,23 @@ data Color = Red | Black deriving Eq
       Tree {
         color   :: Color,
         rbval   :: a,
-        rbleft  :: { v:RedBlackSet {vv:a | vv < rbval} | ((color /= Red) || (getColor v /= Red))},
-        rbright :: { v:RedBlackSet {vv:a | vv > rbval} | ((color /= Red) || (getColor v /= Red)) &&
-                                                         (numBlack v) == (numBlack rbleft)}
+        rbleft  :: { v:RedBlackSet {vv:a | vv < rbval} | RedInvariant color v},
+        rbright :: { v:RedBlackSet {vv:a | vv > rbval} | RedInvariant color v &&
+                                                         BlackInvariant v rbleft}
       }
    @-}
 
-{-@ predicate RedInvariant C S = (C /= Red) || (getColor S /= Red) @-}
+{-@ predicate RedInvariant C S     = (C == Red) ==> (getColor S /= Red) @-}
 {-@ predicate BlackInvariant S0 S1 = (numBlack S0) == (numBlack S1) @-}
 
 data RedBlackSet a =
   Empty |
   Tree {
-    color :: Color,
-    rbval :: a,
+    color   :: Color,
+    rbval   :: a,
     rbleft  :: RedBlackSet a,
     rbright :: RedBlackSet a
   }
-
-testTree :: RedBlackSet Int
-testTree = Tree Red 1 (Tree Black (-1) Empty (Tree Red (0) Empty Empty)) (Tree Black 2 Empty Empty)
 
 {- Assuming that the number of black nodes along are paths to a leaf node are
  - equal, return this value. This function actually counts the number of black
@@ -128,162 +125,146 @@ testTree = Tree Red 1 (Tree Black (-1) Empty (Tree Red (0) Empty Empty)) (Tree B
 {-@ measure numBlack @-}
 {-@ numBlack :: RedBlackSet a -> Nat @-}
 numBlack :: RedBlackSet a -> Int
-numBlack Empty = 0
+numBlack Empty          = 0
 numBlack (Tree c _ l r) = (if c == Black then 1 else 0) + (numBlack l)
 
+{- By definition, the empty nodes in a Red Black Tree are black. The color of
+ - all other nodes is defined by the color field -}
 {-@ measure getColor @-}
 {-@ getColor :: RedBlackSet a -> Color @-}
 getColor Empty          = Black
 getColor (Tree c _ _ _) = c
 
-{-@ predicate IsGTrb N S = isEmptyrb S || N > (toprb S) @-}
-{-@ predicate IsLTrb N S = isEmptyrb S || N < (toprb S) @-}
-
-{-@ measure toprb @-}
-{-@ toprb :: forall a. {v:RedBlackSet a | not (isEmptyrb v)} -> a @-}
-toprb :: RedBlackSet a -> a
-toprb (Tree _ v _ _) = v
-
-{-@ measure isEmptyrb @-}
-{-@ isEmptyrb :: v:RedBlackSet a -> {b:Bool | b <=> Set_emp (setElts  v)} @-}
-isEmptyrb Empty = True
-isEmptyrb _     = False
-
 {-@ instance measure setElts :: forall a. RedBlackSet a -> Set.Set a
-    setElts Empty         = (Set_empty 0)
+    setElts Empty          = (Set_empty 0)
     setElts (Tree _ v l r) = Set_cup (Set_sng v) (Set_cup (setElts l) (setElts r))
   @-}
 
-{- Note about balance with respect to insert:
-     balance is called after insert is recursivly called on one of the subtrees
-     so, we know that the red invariant will hold for the other sub tree (it has
-     not been changed). The black invariant will hold between the subtrees because
-     insert only ever introduces a red node (empty case) or a black node to each
-     subtree (when balancing) (this will probably be hard to prove to liquid).
-
-     We do not know if the red invariant holds for the other subtree.
-
-     The new precondition should say that the red invariant holds for at least
-     one of the subtrees.
- -}
-
-{- OK, I think I see what's going wrong here. Using the algorithm from the book,
- - the balance algorithm takes as arguments red black tree that do not need to
- - maintain the red-invariant. Using liquid, these trees cannot be represented in
- - the first place.
+{- Sometimes, I need to represent a weaker variant of a RedBlackSet. This data
+ - type checks most of the same invariants but, it does not require that the 
+ - red invariant holds for the root of the tree. It does require that the black
+ - invariant holds.
  -
- - I need to modify this function that that it takes some other structure that does
- - not enfore this invariant then, balance will construct from it a red black tree
- - after making sure the invariant holds
- -}
+ - It also a requires a weak version of the red invariant: At least one out of
+ - the root node and its children must be black. This property was chosen
+ - because it holds for trees before and after rebalancing during insertion. -}
 
- {- When balance is called from inside insert, it is after inserting into either
-  - the left or the right subtree (not both!). Importantly, you know which case
-  - it is when calling balance. I plan to right two seperate functions for each case.
-  -
-  - In one case, I will assume that the invariants hold for the left subtree but,
-  - the right subtree will not be a RedBlackSet. Instead, I will pass it as a 
-  - 4-tuple of the arguments used to construct the tree. Hopefully, I'll be able
-  - to massage these argumunts such that the invariants hold. For the other case,
-  - I'll do the same thing but for the right subtree.
-  -}
-
-{-@ data NoRedInvariant a = NoRedInvariant {
-        nricolor   :: Color,
-        nrival   :: a,
-        nrileft  :: RedBlackSet {vv:a | vv < nrival},
-        nriright :: {v:RedBlackSet {vv:a | vv > nrival} |
-          (nricolor /= Red || 
-          (getColor nrileft) /= Red ||
+{-@ data WeakRedInvariant a = WeakRedInvariant {
+        weakColor :: Color,
+        weakVal   :: a,
+        weakLeft  :: RedBlackSet {vv:a | vv < weakVal},
+        weakRight :: {v:RedBlackSet {vv:a | vv > weakVal} |
+          (weakColor /= Red || 
+          (getColor weakLeft) /= Red ||
           (getColor v) /= Red) &&
-          (numBlack v) == (numBlack nrileft)}
+          (numBlack v) == (numBlack weakLeft)}
       }
-   @-}
+  @-}
 
-data NoRedInvariant a = NoRedInvariant {
-  nricolor :: Color,
-  nrival :: a,
-  nrileft :: RedBlackSet a,
-  nriright :: RedBlackSet a
+data WeakRedInvariant a = WeakRedInvariant {
+  weakColor :: Color,
+  weakVal   :: a,
+  weakLeft  :: RedBlackSet a,
+  weakRight :: RedBlackSet a
 }
 
-{-@ predicate NriRedInvariant Nri = (nricolor Nri) /= Red ||
-                                    (getColor (nrileft Nri) /= Red &&
-                                     getColor (nriright Nri) /= Red)
+{- I want to be able to say that a WeakRedInvariant actualy has the original 
+ - strong version of the invariant. -}
+{-@ predicate HasStrongRedInvariant Nri = (weakColor Nri) == Red ==>
+                                            (getColor (weakLeft Nri) /= Red &&
+                                             getColor (weakRight Nri) /= Red)
   @-}
 
-{-@ instance measure setElts :: forall a. NoRedInvariant a -> Set.Set a
-    setElts (NoRedInvariant _ v l r) = Set_cup (Set_sng v) (Set_cup (setElts l) (setElts r))
+{-@ instance measure setElts :: forall a. WeakRedInvariant a -> Set.Set a
+    setElts (WeakRedInvariant _ v l r) = Set_cup (Set_sng v) (Set_cup (setElts l) (setElts r))
   @-}
 
-{-@ measure nrinumBlack @-}
-{-@ nrinumBlack :: NoRedInvariant a -> Nat @-}
-nrinumBlack :: NoRedInvariant a -> Int
-nrinumBlack (NoRedInvariant c _ l r) = (if c == Black then 1 else 0) + (numBlack l)
+{-@ measure weakNumBlack @-}
+{-@ weakNumBlack :: WeakRedInvariant a -> Nat @-}
+weakNumBlack :: WeakRedInvariant a -> Int
+weakNumBlack (WeakRedInvariant c _ l r) = (if c == Black then 1 else 0) + (numBlack l)
 
+{- balanceLeft is called after the recursivly inserting an element into the left
+ - subtree. The recursive insert might have left the tree in a state where the
+ - red invariant no longer holds. This Fixes the invariant in the left subtree
+ - and returns a new tree constructed using the element, right subtree and fixed
+ - left subtree. This new tree might also fail to maintain the red invariant. but,
+ - this will be fixed by later calls to balance or by insert after rb_insert_aux
+ - completes. -}
 {-@ balanceLeft :: forall a. Ord a =>
       c:Color ->
       t:a ->
-
-      l:{v:NoRedInvariant {vv:a | vv < t} | c == Red ==> NriRedInvariant v} ->
-
+      l:{v:WeakRedInvariant {vv:a | vv < t} | c == Red ==> HasStrongRedInvariant v} ->
       r:{v:RedBlackSet {vv:a | vv > t} | RedInvariant c v &&
-                                         (numBlack v) == (nrinumBlack l)} ->
-      {v:NoRedInvariant a | (c /= Red ==> NriRedInvariant v) &&
-                            (nrinumBlack v) == ( if c == Black then 1 else 0) + nrinumBlack l &&
-                            (setElts v) == (Set_cup (Set_sng t) (Set_cup (setElts l) (setElts r)))}
+                                         (numBlack v) == (weakNumBlack l)} ->
+      {v:WeakRedInvariant a | (c /= Red ==> HasStrongRedInvariant v) &&
+                              (weakNumBlack v) == ( if c == Black then 1 else 0) + weakNumBlack l &&
+                              (setElts v) == (Set_cup (Set_sng t) (Set_cup (setElts l) (setElts r)))}
   @-}
-balanceLeft :: Ord a => Color -> a -> NoRedInvariant a -> RedBlackSet a -> NoRedInvariant a
-balanceLeft Black z (NoRedInvariant Red y (Tree Red x a b) c) d =
-  NoRedInvariant Red y (Tree Black x a b) (Tree Black z c d)
+balanceLeft :: Ord a => Color -> a -> WeakRedInvariant a -> RedBlackSet a -> WeakRedInvariant a
+balanceLeft Black z (WeakRedInvariant Red y (Tree Red x a b) c) d =
+  WeakRedInvariant Red y (Tree Black x a b) (Tree Black z c d)
 
-balanceLeft Black z (NoRedInvariant Red x a (Tree Red y b c)) d =
-  NoRedInvariant Red y (Tree Black x a b) (Tree Black z c d)
+balanceLeft Black z (WeakRedInvariant Red x a (Tree Red y b c)) d =
+  WeakRedInvariant Red y (Tree Black x a b) (Tree Black z c d)
 
-balanceLeft c x (NoRedInvariant c' x' a' b' ) b =
-  NoRedInvariant c x (Tree c' x' a' b') b
+balanceLeft c x (WeakRedInvariant c' x' a' b' ) b =
+  WeakRedInvariant c x (Tree c' x' a' b') b
 
+{- balanceRight has the same logic as balanceLeft but, it is called after inserting
+ - into the right subtree. -}
 {-@ balanceRight :: forall a. Ord a =>
       c:Color ->
       t:a ->
       l:{v:RedBlackSet {vv:a | vv < t} | RedInvariant c v} ->
 
-      r:{v:NoRedInvariant {vv:a | vv > t} | (c == Red ==> NriRedInvariant v) &&
-                                            (nrinumBlack v) == (numBlack l)} ->
+      r:{v:WeakRedInvariant {vv:a | vv > t} | (c == Red ==> HasStrongRedInvariant v) &&
+                                              (weakNumBlack v) == (numBlack l)} ->
 
-      {v:NoRedInvariant a | (c /= Red ==> NriRedInvariant v) &&
-                            (nrinumBlack v) == ( if c == Black then 1 else 0) + numBlack l &&
-                            (setElts v) == (Set_cup (Set_sng t) (Set_cup (setElts l) (setElts r)))}
+      {v:WeakRedInvariant a | (c /= Red ==> HasStrongRedInvariant v) &&
+                              (weakNumBlack v) == (if c == Black then 1 else 0) + numBlack l &&
+                              (setElts v) == (Set_cup (Set_sng t) (Set_cup (setElts l) (setElts r)))}
    @-}
-balanceRight :: Ord a => Color -> a -> RedBlackSet a -> NoRedInvariant a -> NoRedInvariant a
-balanceRight Black x a (NoRedInvariant Red z (Tree Red y b c) d ) =
-  NoRedInvariant Red y (Tree Black x a b) (Tree Black z c d)
+balanceRight :: Ord a => Color -> a -> RedBlackSet a -> WeakRedInvariant a -> WeakRedInvariant a
+balanceRight Black x a (WeakRedInvariant Red z (Tree Red y b c) d ) =
+  WeakRedInvariant Red y (Tree Black x a b) (Tree Black z c d)
 
-balanceRight Black x a (NoRedInvariant Red y b (Tree Red z c d) ) =
-  NoRedInvariant Red y (Tree Black x a b) (Tree Black z c d)
+balanceRight Black x a (WeakRedInvariant Red y b (Tree Red z c d) ) =
+  WeakRedInvariant Red y (Tree Black x a b) (Tree Black z c d)
 
-balanceRight c x a (NoRedInvariant c' x' a' b' ) =
-  NoRedInvariant c x a (Tree c' x' a' b')
+balanceRight c x a (WeakRedInvariant c' x' a' b' ) =
+  WeakRedInvariant c x a (Tree c' x' a' b')
 
+{- This is where recursive insertion happens. No special refinements are placed
+ - on the inputs to this function, but the return type is interesting. The return
+ - value is not always a complete red back tree. It only has the full red invariant
+ - if the original color of the tree was black (I'm not sure I fully understand
+ - why this is the case). The other refinements on the return value check that
+ - the black node property has not changed during insertion and that the expected
+ - change has occurred in the set of elements. The first is required when constructing
+ - a new tree from the old subtree and a new subtree where an element has been inserted.
+ - The second is used to ensure this data structure behaves like a set. -}
 {-@ rb_insert_aux :: forall a. Ord a =>
       x:a ->
       s:RedBlackSet a ->
-      {v:NoRedInvariant a | ((getColor s) /= Red ==> NriRedInvariant v) &&
-                            (nrinumBlack v) == (numBlack s) &&
-                            (setElts v) == (Set_cup (Set_sng x) (setElts s))}
+      {v:WeakRedInvariant a | ((getColor s) /= Red ==> HasStrongRedInvariant v) &&
+                              (weakNumBlack v) == (numBlack s) &&
+                              (setElts v) == (Set_cup (Set_sng x) (setElts s))}
   @-}
-rb_insert_aux :: Ord a => a -> RedBlackSet a -> NoRedInvariant a
-rb_insert_aux x Empty = NoRedInvariant Red x Empty Empty
+rb_insert_aux :: Ord a => a -> RedBlackSet a -> WeakRedInvariant a
+rb_insert_aux x Empty = WeakRedInvariant Red x Empty Empty
 rb_insert_aux x (Tree c y a b)
   | x < y     = balanceLeft c y (rb_insert_aux x a) b
   | x > y     = balanceRight c y a (rb_insert_aux x b)
-  | otherwise = (NoRedInvariant c y a b)
+  | otherwise = (WeakRedInvariant c y a b)
 
 instance Ord a => Set RedBlackSet a where
   empty = Empty
 
+  {- The result of rb_insert_aux might not have the red invaraint. This is easy
+   - to fix by forceing the root node to be black -}
   insert x s = forceRedInvarient (rb_insert_aux x s)
-    where forceRedInvarient (NoRedInvariant _ e a b) = Tree Black e a b
+    where forceRedInvarient (WeakRedInvariant _ e a b) = Tree Black e a b
 
   member _ Empty = False
   member e (Tree _ x l r)
